2c2dc6f8163868391077c2841531b9db4f9146e9
euuns <dmsu2@naver.com>
2025-05-27 03:46:30 +0900

    Core: Create Project

---
+++ src/main/java/com/example/harudam/HarudamApplication.java
@@ -0,0 +1,13 @@
+package com.example.harudam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarudamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarudamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/harudam/HarudamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.harudam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarudamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
